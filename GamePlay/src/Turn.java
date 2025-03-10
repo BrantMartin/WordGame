5121dc7bf6d5bd14e6d4d282fb97d9fb209c4414
Brant Martin <100722069+BrantMartin@users.noreply.github.com>
2025-03-09 17:45:28 -0700

    initial commit

--- GamePlay/src/Turn.java
+++ GamePlay/src/Turn.java
@@ -44,14 +44,14 @@ public class Turn {
                 try {
                     monPrize.setMoney(monPrize.displayWinnings(player, host.findLetters(guess)));
                 } catch (Exception e) {
-                    System.out.println("You entered more than one letter.");
+                    JOptionPane.showConfirmDialog(null, "You entered more than one letter.");
                 }
                 return true;
             } else {
                 try {
                     monPrize.setMoney(monPrize.displayWinnings(player, host.findLetters(guess)));
                 } catch (Exception e) {
-                    System.out.println("You entered more than one letter.");
+                    JOptionPane.showConfirmDialog(null, "You entered more than one letter.");
                 }
                 return false;
             }
