c6029dc31909911d0d26684f60257b1874c61910
Brant Martin <100722069+BrantMartin@users.noreply.github.com>
2025-03-27 17:36:30 -0700

    finished project up to scroll bar. I am not adding the scrollbar because I think it makes the game worse.

--- GamePlay/src/GUI.java
+++ GamePlay/src/GUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class GUI {
     public static Players[] currentPlayers = new Players[3];
@@ -12,27 +13,73 @@ public class GUI {
         Turn gameTurn = new Turn();
 
         JFrame aFrame = new JFrame("Word Game");
-        aFrame.setSize(500, 500);
+        aFrame.setSize(800, 300);
         aFrame.setVisible(true);
+        aFrame.setLayout(new GridLayout());
         aFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JLabel playersLabel = new JLabel("Player 1: " + currentPlayers[0] + "\nPlayer 2: " + currentPlayers[1]
-                + "\nPlayer 3: " + currentPlayers[2]);
-        JLabel hostLabel = new JLabel("Host: " + host.getFirstName() + host.getLastName());
-        JLabel gamePhraseLabel = new JLabel("The phrase to guess is: " + host.getPlayingPhrase());
-        JButton playerButton = new JButton("Add Player");
-        JButton hostButton = new JButton("Add Host");
-        JButton turnButton = new JButton("Start Turn");
+        aFrame.setLocationRelativeTo(null);
+        
+        JPanel playerPanel = new JPanel(new GridBagLayout());
+        JPanel hostPanel = new JPanel(new GridBagLayout());
+        JPanel startPanel = new JPanel(new GridBagLayout());
+        
 
-        aFrame.setLayout(new FlowLayout());
-        aFrame.add(playersLabel);
-        aFrame.add(playerButton);
-        aFrame.add(hostLabel);
-        aFrame.add(gamePhraseLabel);
-        aFrame.add(hostButton);
-        aFrame.add(turnButton);
+        JLabel playerOneLabel = new JLabel("Player 1: " + currentPlayers[0]);
+        JLabel playerTwoLabel = new JLabel("Player 2: " + currentPlayers[1]);
+        JLabel playerThreeLabel = new JLabel("Player 3: " + currentPlayers[2]);
+        JLabel hostLabel = new JLabel("Host: " + host.getFirstName() + host.getLastName());
+        JLabel gamePhraseLabel = new JLabel("Phrase: " + host.getPlayingPhrase());
+
+        JButton turnButton = new JButton("Start Turn");
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu gameMenu = new JMenu("Game");
+        JMenuItem addPlayerItem = new JMenuItem("Add Player(s)");
+        JMenuItem addHostItem = new JMenuItem("Add Host");
+        JMenu aboutMenu = new JMenu("About");
+        JMenuItem layoutItem = new JMenuItem("Layout");
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        playerPanel.add(playerOneLabel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        playerPanel.add(playerTwoLabel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        playerPanel.add(playerThreeLabel, gbc);
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        hostPanel.add(hostLabel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        hostPanel.add(gamePhraseLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        startPanel.add(turnButton, gbc);
+        
+        aFrame.add(playerPanel);
+        aFrame.add(hostPanel);
+        aFrame.add(startPanel);
+        
+        aFrame.setJMenuBar(menuBar);
+        menuBar.add(gameMenu);
+        gameMenu.add(addPlayerItem);
+        gameMenu.add(addHostItem);
+        menuBar.add(aboutMenu);
+        aboutMenu.add(layoutItem);
+        gameMenu.setMnemonic(KeyEvent.VK_G);
+        aboutMenu.setMnemonic(KeyEvent.VK_A);
+        
         SwingUtilities.updateComponentTreeUI(aFrame);
 
-        playerButton.addActionListener(_ -> {
+        addPlayerItem.addActionListener(_ -> {
             for (int i = 0; i < currentPlayers.length; i++) {
                 String fName = JOptionPane.showInputDialog("Please enter your first name Player #" + (i + 1));
                 String lName = JOptionPane.showInputDialog("Would you like to enter a last name? Leave blank if not.");
@@ -42,19 +89,24 @@ public class GUI {
                     currentPlayers[i].setFirstName(fName);
                     currentPlayers[i].setLastName(lName);
                 }
-                playersLabel.setText("Player 1: " + currentPlayers[0] + "\nPlayer 2: " + currentPlayers[1]
-                        + "\nPlayer 3: " + currentPlayers[2]);
+                playerOneLabel.setText("Player 1: " + currentPlayers[0]);
+                playerTwoLabel.setText("Player 2: " + currentPlayers[1]);
+                playerThreeLabel.setText("Player 3: " + currentPlayers[2]);
                 SwingUtilities.updateComponentTreeUI(aFrame);
             }
         });
 
-        hostButton.addActionListener(_ -> {
+        addHostItem.addActionListener(_ -> {
             host.setFirstName(JOptionPane.showInputDialog("Enter the host's first name"));
             host.setLastName(JOptionPane.showInputDialog("Enter the host's last name"));
             host.setPhrase(JOptionPane.showInputDialog("What will the phrase to guess be"));
             hostLabel.setText("Host: " + host.getFirstName() + " " + host.getLastName());
-            gamePhraseLabel.setText("The phrase to guess is: " + host.getPlayingPhrase());
+            gamePhraseLabel.setText("Phrase: " + host.getPlayingPhrase());
             SwingUtilities.updateComponentTreeUI(aFrame);
+        });
+        
+        layoutItem.addActionListener(_ -> {
+            JOptionPane.showMessageDialog(null, "I used the GridBagLayout because I was able to have more control\nover the layout of my application through the options that it provides.");
         });
 
         turnButton.addActionListener(_ -> {
